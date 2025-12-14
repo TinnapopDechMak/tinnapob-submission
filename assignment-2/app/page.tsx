a5d8f74f2b363fdfac1110f57d31f50b556b2d61
tinnapop dechmak <60779574+TinnapopDechMak@users.noreply.github.com>
2025-12-14 10:59:55 +0700

    Create initial transaction dashboard summary and transaction list

--- assignment-2/app/page.tsx
+++ assignment-2/app/page.tsx
@@ -26,13 +26,57 @@ export default function Page() {
   }
 
   const balance = totalIncome - totalExpense;
+
   const sorted = [...transactions].sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
 
   return (
     <main>
+      <div>
+        <h1>
+          Transaction Dashboard
+        </h1>
 
+        {/* Sum */}
+        <section>
+          <div>
+            <div>Total Income</div>
+            <div>{formatTHB(totalIncome)}</div>
+          </div>
+
+          <div>
+            <div>Total Expenses</div>
+            <div>{formatTHB(totalExpense)}</div>
+          </div>
+
+          <div>
+            <div>Balance</div>
+            <div>{formatTHB(balance)}</div>
+          </div>
+        </section>
+
+        {/* list */}
+        <section>
+          {sorted.map((t) => {
+            const isIncome = t.type === "income";
+            return (
+              <div
+                key={t.id}
+              >
+                <div>
+                  <div >{t.description}</div>
+                  <div >{formatDate(t.date)}</div>
+                </div>
+
+                <div>
+                  {isIncome ? "+" : "-"}{formatTHB(t.amount)}
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
     </main>
   );
 }
