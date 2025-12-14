b90a272cdb6f49047cf2affb6aa6591f874bd65f
tinnapop dechmak <60779574+TinnapopDechMak@users.noreply.github.com>
2025-12-14 11:21:13 +0700

    Refine dashboard UI

--- assignment-2/app/page.tsx
+++ assignment-2/app/page.tsx
@@ -32,44 +32,45 @@ export default function Page() {
   );
 
   return (
-    <main>
-      <div>
-        <h1>
+    <main className="min-h-screen bg-gray-900 text-gray-200">
+      <div className="mx-auto max-w-4xl p-4">
+        <h1 className="mb-4 text-center text-2xl font-bold">
           Transaction Dashboard
         </h1>
 
         {/* Sum */}
-        <section>
-          <div>
-            <div>Total Income</div>
-            <div>{formatTHB(totalIncome)}</div>
+        <section className="grid grid-cols-3 divide-x rounded-md border bg-white">
+          <div className="p-4 text-center">
+            <div className="text-gray-500">Total Income</div>
+            <div className="text-green-500">{formatTHB(totalIncome)}</div>
           </div>
 
-          <div>
-            <div>Total Expenses</div>
-            <div>{formatTHB(totalExpense)}</div>
+          <div className="p-4 text-center">
+            <div className="text-gray-500">Total Expenses</div>
+            <div className="text-red-500">{formatTHB(totalExpense)}</div>
           </div>
 
-          <div>
-            <div>Balance</div>
-            <div>{formatTHB(balance)}</div>
+          <div className="p-4 text-center">
+            <div className="text-gray-500">Balance</div>
+            <div className="text-gray-900">{formatTHB(balance)}</div>
           </div>
         </section>
 
         {/* list */}
-        <section>
+        <section className="mt-5 rounded-md bg-white">
           {sorted.map((t) => {
             const isIncome = t.type === "income";
             return (
               <div
                 key={t.id}
+                className="flex flex-col gap-1 p-3 border-b"
               >
                 <div>
-                  <div >{t.description}</div>
-                  <div >{formatDate(t.date)}</div>
+                  <div className="text-gray-500">{t.description}</div>
+                  <div className="text-gray-500">{formatDate(t.date)}</div>
                 </div>
 
-                <div>
+                <div className={"text-right text-base font-bold " + (isIncome ? "text-green-500" : "text-red-500")}>
                   {isIncome ? "+" : "-"}{formatTHB(t.amount)}
                 </div>
               </div>
